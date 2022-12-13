822af341a42857af1c129528bc2cc4b09bd05700
Joe Byrne <joebyrne709@gmail.com>
2022-12-14 08:33:38 +1300

    Update EEG plot fuctionality for multiple channels

--- Scripts/TimeSeriesPlot.m
+++ Scripts/TimeSeriesPlot.m
@@ -1,12 +1,33 @@
+% Retrieve and display EEG data from a patient
+% Author: Joe Byrne
+
 clear, clc
 close all
 
-db = load('C:\Users\joeby\Proj-SleepAnalysisSRS\Database\P1\Data.mat');
 
-EKG = db.EKG;
-EKG_r = reshape(EKG, [1, size(EKG,1)*size(EKG,2)]);
-TIME = linspace(0, size(db.TimeStep,2)*10, size(EKG,1)*size(EKG,2));
+% Vars
+channel_names  = ["Ffour_Mone"  , "Fthree_Mtwo" , "Cfour_Mone" , ... 
+                  "Cthree_Mtwo" , "Otwo_Mone"   , "Oone_Mtwo"   ];
+channel_labels = ["F4-M1" , "F3-M2" , "C4-M1" , ...
+                  "C3-M2" , "O2-M1" , "O1-M2"  ];
+timestep = load('..\Data\Database\P2\Data.mat', 'TimeStep');
 
-plot(TIME, EKG_r)
-xlabel('Time (s)')
-ylabel('EKG Reading (V)')
+% Fig setup
+figure('units','normalized','outerposition',[0 0 1 1])
+tiledlayout(3,2);
+
+for ii = 1:length(channel_names)
+    % Get data
+    eeg = load('..\Data\Database\P2\Data.mat', channel_names(ii)).(channel_names(ii));
+    EEG{ii} = reshape(eeg, [1, size(eeg,1)*size(eeg,2)]);
+    TIME = linspace(0, size(timestep,2)*10, size(EEG{ii},2)) / 3600;
+    
+    % Plot data
+    nexttile
+    plot(TIME, EEG{ii})
+    title(sprintf("EEG: Channel %s", channel_labels(ii)))
+    ylabel('EKG Reading (\muV)')
+    if ii > length(channel_names) - 2
+        xlabel('Time (h)')
+    end
+end
